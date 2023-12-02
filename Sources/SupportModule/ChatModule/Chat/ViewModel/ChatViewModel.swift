bb72d0eb70a0468617009068c040cc9591eee6b2
Danilo Hernandez <jhernandez@tribalworldwide.gt>
2023-12-01 19:34:04 -0500

    Update

--- Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
+++ Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
@@ -107,19 +107,23 @@ class ChatViewModel: ObservableObject {
             .collection(FirebaseConstants.messages)
             .document(fromUUID)
         
+        DispatchQueue.global().async {
+            
+       
         do {
-            dbFirestore.collection(FirebaseConstants.lastMessages)
+            self.dbFirestore.collection(FirebaseConstants.lastMessages)
                 .document(fromUUID)
                 .collection(FirebaseConstants.messages)
                 .document(toUUID).setData(message)
             
-            dbFirestore.collection(FirebaseConstants.lastMessages)
+            self.dbFirestore.collection(FirebaseConstants.lastMessages)
                 .document(toUUID)
                 .collection(FirebaseConstants.messages)
                 .document(fromUUID).setData(message)
 
         }catch  let error{
             print("Error saving message")
+        }
         }
         
 //        let batch = dbFirestore.batch()
