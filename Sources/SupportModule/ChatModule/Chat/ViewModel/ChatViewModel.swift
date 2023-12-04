80d6131d76140de80caf6b09916db02d5505c6c0
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-04 11:18:24 -0500

    update

--- Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
+++ Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
@@ -104,15 +104,15 @@ class ChatViewModel: ObservableObject {
             .document(fromUUID)
         
         do {
-             dbFirestore.collection(FirebaseConstants.lastMessages)
-                .document(toUUID)
-                .collection(FirebaseConstants.messages)
-                .document(fromUUID).setData(message)
+//             dbFirestore.collection(FirebaseConstants.lastMessages)
+//                .document(toUUID)
+//                .collection(FirebaseConstants.messages)
+//                .document(fromUUID).setData(message)
             
-//            dbFirestore.collection(FirebaseConstants.lastMessages)
-//               .document(fromUUID)
-//               .collection(FirebaseConstants.messages)
-//               .document(toUUID).setData(message)
+            dbFirestore.collection(FirebaseConstants.lastMessages)
+               .document(fromUUID)
+               .collection(FirebaseConstants.messages)
+               .document(toUUID).setData(message)
 
         }catch  let error{
             print("Error saving message")
