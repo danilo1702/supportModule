95482253c34f680fb662e700b4d1af50e72f3bd5
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-05 11:50:25 -0500

    Update

--- Sources/SupportModule/ChatModule/Chat/View/ChatView.swift
+++ Sources/SupportModule/ChatModule/Chat/View/ChatView.swift
@@ -22,6 +22,8 @@ public struct ChatView: View {
                 ScrollViewReader { scrollViewProxy in
                     VStack{
                         ForEach(viewModel.messages, id: \.uniqueID) { message in
+                            Text("From\(message.fromUUID)")
+                            Text("to \(message.toUUID)")
                            BumbleChat(message: message)
                         }
                         HStack {
