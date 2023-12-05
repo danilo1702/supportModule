47ce543617d93514c1ea098e70f710e3ca11e335
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-05 12:02:11 -0500

    update

--- Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
+++ Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
@@ -64,7 +64,7 @@ class ChatViewModel: ObservableObject {
             .collection(toUUID)
             .document()
 
-        let message = ["message": "message", "fromUUID": "fromUUID", "toUUID": toUUID, "timestamp": date, "fromName": "UIDevice.modelName"] as [String: Any]
+        let message = ["message": message, "fromUUID": "fromUUID", "toUUID": toUUID, "timestamp": date, "fromName": "UIDevice.modelName"] as [String: Any]
         referenceSender.setData(message) { error in
             if error != nil {
                 print("Errro sending de message ")
