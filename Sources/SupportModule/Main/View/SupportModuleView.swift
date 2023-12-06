5e935251ffccbcc4679a4ada81c5490583f12c5d
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-06 18:11:59 -0500

    as

--- Sources/SupportModule/Main/View/SupportModuleView.swift
+++ Sources/SupportModule/Main/View/SupportModuleView.swift
@@ -72,6 +72,9 @@ public struct SupportModuleView: View {
                     .shadow(radius: 5)
                    navigationLinks()
                 }
+                .onChange(of: chatHistory) { newValue in
+                    print("new SUPOORT \(newValue)")
+                }
                 .addSearchbar(textSearch: $textSearch, placeHolder: generalConfiguration.placeHolderSearchBar, title: generalConfiguration.titleModule)
                 .onAppear{
                     viewModel.registerUserFirebase{ result in
