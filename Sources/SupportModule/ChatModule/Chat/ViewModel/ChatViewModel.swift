e89ee4db06e762bd6025af7ae507f57aa9330928
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-06 17:08:36 -0500

    a

--- Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
+++ Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
@@ -86,7 +86,6 @@ class ChatViewModel: ObservableObject {
         guard let fromUUID = Auth.auth().currentUser?.uid else { return }
         
         let date = String(DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short))
-        self.tryThis(text: message)
         let referenceSender = dbFirestore.collection(FirebaseConstants.messages)
             .document(fromUUID)
             .collection(toUUID)
