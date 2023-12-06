35c10f080c1e6595acae3bb8801042c5e7326112
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-06 16:50:08 -0500

    messag

--- Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
+++ Sources/SupportModule/ChatModule/Chat/ViewModel/ChatViewModel.swift
@@ -72,8 +72,8 @@ class ChatViewModel: ObservableObject {
         
         let bath = dbFirestore.batch()
         
-        bath.setData(["Mensaje": text,"fromUUID": fromUUID ,"toUUID": toUUID, "fromName": UIDevice.modelName, "timestamp": date], forDocument: senderReference)
-        bath.setData(["Mensaje": text,"fromUUID": fromUUID,"toUUID": toUUID, "fromName": UIDevice.modelName, "timestamp": date], forDocument: receiverReference)
+        bath.setData(["message": text,"fromUUID": fromUUID, "toUUID": toUUID, "fromName": UIDevice.modelName, "timestamp": date], forDocument: senderReference)
+        bath.setData(["message": text,"fromUUID": fromUUID, "toUUID": toUUID, "fromName": UIDevice.modelName, "timestamp": date], forDocument: receiverReference)
         bath.commit { error in
             if error == nil {
             print("Guardado")
