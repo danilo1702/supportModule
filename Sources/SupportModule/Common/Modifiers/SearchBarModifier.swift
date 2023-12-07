75f6368da2cdaf787a9195b8cf4156d24a267e3c
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-07 17:17:07 -0500

    update

--- Sources/SupportModule/Common/Modifiers/SearchBarModifier.swift
+++ Sources/SupportModule/Common/Modifiers/SearchBarModifier.swift
@@ -1,6 +1,6 @@
 //
 //  SearchBarModifier.swift
-//  
+//
 //
 //  Created by Danilo Hernandez on 30/10/23.
 //
@@ -24,50 +24,53 @@ public struct SearchBarModifier: ViewModifier {
     
     
     public func body(content: Content) -> some View {
-         NavigationView {
-             ScrollView {
-            VStack {
-                if let text = title {
-                    TextView(informationModel: text)
-                        .shadow(radius: 6)
-                }
-                
-                ZStack {
-                    HStack {
-                        Image(systemName: CommonStrings.ImagesString.magnifyingGlass)
-                            .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: -2))
-                            .foregroundStyle(.gray)
-                        if !hidePlaceHolder || textSearch.isEmpty{
-                            Text(placeHolder)
-                                .foregroundStyle(.gray)
-                        }
-                        
-                        Spacer()
+        NavigationView {
+            ScrollView {
+                VStack {
+                    if let text = title {
+                        TextView(informationModel: text)
+                            .shadow(radius: 6)
                     }
                     HStack {
-                        TextField(CommonStrings.emptyString, text: $textSearch).padding(EdgeInsets(top: 8, leading: 33, bottom: 8, trailing: 5))
+                        
+                        ZStack {
+                            HStack {
+                                Image(systemName: CommonStrings.ImagesString.magnifyingGlass)
+                                    .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: -2))
+                                    .foregroundStyle(.gray)
+                                if !hidePlaceHolder || textSearch.isEmpty{
+                                    Text(placeHolder)
+                                        .foregroundStyle(.gray)
+                                }
+                                
+                                Spacer()
+                            }
+                            
+                            TextField(CommonStrings.emptyString, text: $textSearch).padding(EdgeInsets(top: 8, leading: 33, bottom: 8, trailing: 5))
+                            
+                            
+                            
+                            
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .strokeBorder(.gray, lineWidth: 0.2)
+                                .background(.gray.opacity(0.2)))
+                        .cornerRadius(CGFloat( 15.0))
+                        .padding()
+                        .onTapGesture {
+                            hidePlaceHolder = true
+                        }
                         if !textSearch.isEmpty {
                             Button(action: { completion() }, label: {
                                 Text("Buscar")
                             })
                         }
                     }
-                    
-                    
+                    Spacer()
+                    content
                 }
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .strokeBorder(.gray, lineWidth: 0.2)
-                        .background(.gray.opacity(0.2)))
-                .cornerRadius(CGFloat( 15.0))
-                .padding()
-                .onTapGesture {
-                    hidePlaceHolder = true
-                }
-                Spacer()
-                content
-            }
-        }}
+            }}
     }
 }
 
