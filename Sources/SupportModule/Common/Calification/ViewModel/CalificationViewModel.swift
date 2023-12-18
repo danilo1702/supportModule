550cf6eebe0238b8d29a91aad60cf22772ad1804
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-18 17:53:35 -0500

    as

--- Sources/SupportModule/Common/Calification/ViewModel/CalificationViewModel.swift
+++ Sources/SupportModule/Common/Calification/ViewModel/CalificationViewModel.swift
@@ -71,7 +71,7 @@ public class CalificationViewModel: ObservableObject {
     }
     func sendInformation(information: SendCalification, toUUID: String, completion: @escaping (Result<Bool, Never>) -> ()) {
         guard let uuid = FirebaseManagerData.initialization.dbAuth.currentUser?.uid else { return }
-        let reference = FirebaseManagerData.initialization.dbFirestore.collection(FirebaseConstants.closedChats).document(uuid).collection(FirebaseConstants.messages).document(toUUID)
+        let reference = FirebaseManagerData.initialization.dbFirestore.collection(FirebaseConstants.closedChats).document(uuid).collection(FirebaseConstants.calification).document(toUUID)
         
         if let result = try? reference.setData(from: information) {
             completion(.success(true))
