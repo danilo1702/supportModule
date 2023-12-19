262df0754b39737e0575dc8e2749ae5bb91f6d7f
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-19 16:36:58 -0500

    as

--- Sources/SupportModule/Common/Modifiers/SearchBarModifier.swift
+++ Sources/SupportModule/Common/Modifiers/SearchBarModifier.swift
@@ -27,72 +27,73 @@ public struct SearchBarModifier: ViewModifier {
     
     public func body(content: Content) -> some View {
         NavigationView {
-            ScrollView {
-                ZStack{
-                    ShapeMainView()
-                        .fill(.blue)
-                        .ignoresSafeArea()
-                        .shadow(radius: 20)
-                    VStack {
-                        if let text = title {
-                            HStack {
-                                TextView(informationModel: text)
-                                    .shadow(radius: 6)
-                                Spacer()
-                            }.padding()
-                        }
-                        HStack {
-                            
-                            ZStack {
-                                HStack {
-                                    Image(systemName: CommonStrings.ImagesString.magnifyingGlass)
-                                        .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: -2))
-                                        .foregroundStyle(.gray)
-                                    if !hidePlaceHolder || textSearch.isEmpty{
-                                        Text(placeHolder)
-                                            .foregroundStyle(.gray)
-                                    }
-                                    
-                                    Spacer()
-                                }
-                                
-                                TextField(CommonStrings.emptyString, text: $textSearch).padding(EdgeInsets(top: 8, leading: 33, bottom: 8, trailing: 5))
-                                
-                            }
-                            .background(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .strokeBorder(.gray, lineWidth: 0.2)
-                                    .background(.white))
-                            .cornerRadius(CGFloat( 15.0))
-                            .padding(.init(top: 20, leading: 20, bottom: 20, trailing: textSearch.isEmpty ? 20 : 10))
-                            .onTapGesture {
-                                hidePlaceHolder = true
-                            }
-                            if !textSearch.isEmpty {
-                                Button(action: {
-                                    withAnimation(.smooth) {
-                                        actionButton()
-                                    }
-                                    
-                                }, label: {
-                                    Text( searching ? "Cancelar" : "Buscar")
-                                        .padding(.init(top: 20, leading: 0, bottom: 20, trailing: 10))
-                                })
-                            }
-                        }
-                        .onChange(of: textSearch, perform: { value in
-                            if value.isEmpty || oldValue != value{
-                                searching = false
-                            }
-                        })
+            
+            ZStack{
+                ShapeMainView()
+                    .fill(.blue)
+                    .ignoresSafeArea()
+                    .shadow(radius: 20)
+                ScrollView {
+                   
                         
-                        Spacer()
-                        content
-                            .padding(.top, 15)
-                    }
+                        VStack {
+                            if let text = title {
+                                HStack {
+                                    TextView(informationModel: text)
+                                        .shadow(radius: 6)
+                                    Spacer()
+                                }.padding()
+                            }
+                            HStack {
+                                
+                                ZStack {
+                                    HStack {
+                                        Image(systemName: CommonStrings.ImagesString.magnifyingGlass)
+                                            .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: -2))
+                                            .foregroundStyle(.gray)
+                                        if !hidePlaceHolder || textSearch.isEmpty{
+                                            Text(placeHolder)
+                                                .foregroundStyle(.gray)
+                                        }
+                                        
+                                        Spacer()
+                                    }
+                                    
+                                    TextField(CommonStrings.emptyString, text: $textSearch).padding(EdgeInsets(top: 8, leading: 33, bottom: 8, trailing: 5))
+                                    
+                                }
+                                .background(
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .strokeBorder(.gray, lineWidth: 0.2)
+                                        .background(.white))
+                                .cornerRadius(CGFloat( 15.0))
+                                .padding(.init(top: 20, leading: 20, bottom: 20, trailing: textSearch.isEmpty ? 20 : 10))
+                                .onTapGesture {
+                                    hidePlaceHolder = true
+                                }
+                                if !textSearch.isEmpty {
+                                    Button(action: {
+                                        withAnimation(.smooth) {
+                                            actionButton()
+                                        }
+                                        
+                                    }, label: {
+                                        Text( searching ? "Cancelar" : "Buscar")
+                                            .padding(.init(top: 20, leading: 0, bottom: 20, trailing: 10))
+                                    })
+                                }
+                            }
+                            .onChange(of: textSearch, perform: { value in
+                                if value.isEmpty || oldValue != value{
+                                    searching = false
+                                }
+                            })
+                            
+                            Spacer()
+                            content
+                        }
                 }
-                
-            }.ignoresSafeArea()
+            }
         }.ignoresSafeArea()
     }
     func actionButton() -> () {
