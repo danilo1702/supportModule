0714912622dadabbdb29a6cf110fae213d573ab4
Danilo Hernandez <diaz-danilo@hotmail.com>
2023-12-21 14:03:30 -0500

    ss

--- Sources/SupportModule/DynamicModule/SubViews/SelectOption/View/SelectOptionView.swift
+++ Sources/SupportModule/DynamicModule/SubViews/SelectOption/View/SelectOptionView.swift
@@ -70,7 +70,7 @@ struct SelectOptionViewV2: View {
             }
         }
         .padding()
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .leading)
+        .frame(width: .infinity, height: .infinity, alignment: .leading)
     }
     @ViewBuilder
     func showItems(_ item: OptionsMessageModel) -> some View {
