265fc2bad16fcf30e0213349194c8a3751683df9
Danilo Hernandez <diaz-danilo@hotmail.com>
2024-01-28 10:51:08 -0500

    update

--- Sources/SupportModule/DynamicModule/SubViews/Draw/ViewModel/DrawingViewModelChat.swift
+++ Sources/SupportModule/DynamicModule/SubViews/Draw/ViewModel/DrawingViewModelChat.swift
@@ -19,56 +19,47 @@ public class DrawingViewModelChat : ObservableObject {
     }
     
     func saveImageToPhotoLibrary(_ image: UIImage, lines: [LineModel]) async {
-        var downloadURL: URL?
         guard let fromUUID = FirebaseManagerData.initialization.dbAuth.currentUser?.uid else { return }
         let chat = FormTypeMessageViewModel(toUUID: messageModel.fromUUID, messageModel: messageModel)
         let storage = Storage.storage()
         let storageRef = storage.reference()
         let uuid = UUID().uuidString
         
-         uploadImage(image: image, storageRef: storageRef, uuid: uuid)
-//        do {
-//            
-//          let url =  try await uploadImage(image: image, storageRef: storageRef, uuid: uuid) { completion in
-//                switch completion {
-//                    case .success(let url):
-//                       
-//                            let option = [OptionsMessage(id: UUID().uuidString, lines: lines)]
-//                                await chat.sendMessage(message: "\(url)", type: TypeMessage.image.rawValue, options:  option) { result in
-//                                     switch result {
-//                                         case .success(let success):
-//                                             print("IMAGEN GUARDADA")
-//                                             
-//                                             await chat.updateMessage(message: self.messageModel.message, type: TypeMessage.signature.rawValue, options: option) { completion in
-//                                                 switch completion {
-//                                                     case .success(let success):
-//                                                         print(success)
-//                                                     case .failure(let failure):
-//                                                         print(failure)
-//                                                 }
-//                                             }
-//                                         case .failure(let failure):
-//                                             print(failure)
-//                                     }
-//                                 }
-//                       
-//                    case .failure(let failure):
-//                        print(failure)
-//                }
-//            }
-//        }catch {
-//            print(error)
-//        }
-       
-       
-        
-         }
+        try await uploadImage(image: image, storageRef: storageRef, uuid: uuid) { completion in
+            switch completion {
+                case .success(let url):
+                    
+                    let option = [OptionsMessage(id: UUID().uuidString, lines: lines)]
+                    await chat.sendMessage(message: "\(url)", type: TypeMessage.image.rawValue, options:  option) { result in
+                        switch result {
+                            case .success(let success):
+                                print("IMAGEN GUARDADA \(success)")
+                                
+                                await chat.updateMessage(message: self.messageModel.message, type: TypeMessage.signature.rawValue, options: option) { completion in
+                                    switch completion {
+                                        case .success(let success):
+                                            print(success)
+                                        case .failure(let failure):
+                                            print(failure)
+                                    }
+                                }
+                            case .failure(let failure):
+                                print(failure)
+                        }
+                    }
+                    
+                case .failure(let failure):
+                    print(failure)
+            }
+        }
+    }
     
-    func uploadImage(image:UIImage, storageRef: StorageReference, uuid: String)  {
+    func uploadImage(image:UIImage, storageRef: StorageReference, uuid: String, completion: (Result<URL, Error>) async -> ()) async {
         var urlImage: URL?
         // Create a reference to the file you want to upload
         let riversRef = storageRef.child("messageImage/\(uuid).jpg")
-        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return  }
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { await completion(.failure(URLError(.fileDoesNotExist)))
+            return }
         let uploadMeta = StorageMetadata.init()
         uploadMeta.contentType = "image/jpg"
         
@@ -83,10 +74,13 @@ public class DrawingViewModelChat : ObservableObject {
                     return
                 }
                 urlImage = url
-                print("\(String(describing: urlImage))")
             }
         }
+        guard  let url = urlImage else {
+            await completion(.failure(URLError(.badURL)))
+            return   }
         
+        await completion(.success(url))
     }
     func showLineasFromMessage() {
         guard let options = messageModel.options else { return }
