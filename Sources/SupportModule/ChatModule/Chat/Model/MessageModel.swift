bc7d10cf7f9d1518890693c5e9061f462a23672e
Danilo Hernandez <diaz-danilo@hotmail.com>
2024-01-28 14:09:46 -0500

    updatre

--- Sources/SupportModule/ChatModule/Chat/Model/MessageModel.swift
+++ Sources/SupportModule/ChatModule/Chat/Model/MessageModel.swift
@@ -10,9 +10,7 @@ import FirebaseFirestoreSwift
 import FirebaseFirestore
 import Drawing
 import SwiftUI
-
 public struct MessageModel: Codable, Identifiable {
-    
     public var uniqueID: UUID = UUID()
     @DocumentID public var id: String?
     let message: String
@@ -24,7 +22,7 @@ public struct MessageModel: Codable, Identifiable {
     let type: String
     let options: [OptionsMessageModel]?
     
-    public init(id: String? = nil, message: String, fromUUID: String, toUUID: String, timestamp: Timestamp? = nil, fromName: String, messageRead: Bool? = false, type: String, options: [OptionsMessageModel]? = nil) {
+    public init( id: String? = nil, message: String, fromUUID: String, toUUID: String, timestamp: Timestamp? = nil, fromName: String, messageRead: Bool? = nil, type: String, options: [OptionsMessageModel]? = nil) {
         self.id = id
         self.message = message
         self.fromUUID = fromUUID
@@ -35,6 +33,7 @@ public struct MessageModel: Codable, Identifiable {
         self.type = type
         self.options = options
     }
+    
     enum CodingKeys: CodingKey {
         case id
         case message
@@ -49,42 +48,49 @@ public struct MessageModel: Codable, Identifiable {
 }
 
 public struct OptionsMessageModel: Codable, Hashable {
-    
-    
     let id: String
     let text: String
     let lines: linesModelDrwApi?
     
+    enum CodingKeys: String, CodingKey {
+        case id
+        case text
+        case lines
+    }
+
     public init(text: String, lines: linesModelDrwApi? = nil, id: String) {
         self.text = text
         self.lines = lines
         self.id = id
     }
+
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
     }
+
     public static func == (lhs: OptionsMessageModel, rhs: OptionsMessageModel) -> Bool {
         return lhs.id == rhs.id
-        }
+    }
 }
 
 public struct linesModelDrwApi: Codable {
     public var points: [PointsLineApi]
     public var color: String
     public var lineWidth: Double
-    public init( color: String, lineWidth: Double, points: [PointsLineApi]) {
+
+    public init(color: String, lineWidth: Double, points: [PointsLineApi]) {
         self.points = points
         self.color = color
         self.lineWidth = lineWidth
     }
 }
+
 public struct PointsLineApi: Codable {
     public var x: String
     public var y: String
-    
+
     public init(x: String, y: String) {
         self.x = x
         self.y = y
     }
 }
-
